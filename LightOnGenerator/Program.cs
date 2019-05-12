e400c9b78240863c5a2ed43c97e2b07e49b22dc3
mhamad98 <46487299+mhamad98@users.noreply.github.com>
2019-05-13 00:14:10 +0300

    conf
    abelity to add levels between 4 and 12

--- LightOnGenerator/Program.cs
+++ LightOnGenerator/Program.cs
@@ -7,7 +7,7 @@ namespace LightOnGenerator
     class Program
     {
         static int low = 4;
-        static int height = 8;
+        static int height = 12;
 
         static void printList(List<List<int>> cases)
         {
@@ -28,51 +28,64 @@ namespace LightOnGenerator
         {
             int n = 0;
             bool repeat = true;
+            bool rep = true;
 
-            do
-            {
-                Console.WriteLine("Enter the number of lights ("+low+" -- "+height+"): ");
-                var tem = Console.ReadLine();
-                n = int.Parse(tem);
-            } while (n < low || n > height);
-            
-            while (repeat)
+            while (rep)
             {
                 Console.Clear();
-                Generator g = new Generator(n, 0);
-                Console.WriteLine("This is the level generated: ");
-                printList(g.generate());
-                Console.WriteLine("Do you want to change it? (y or n): ");
-                string tem;
                 do
                 {
-                    tem = Console.ReadLine();
-                } while (tem[0] != 'y' && tem[0] != 'n');
-                repeat = (tem[0] == 'y');
-            }
+                    Console.WriteLine("Enter the number of lights (" + low + " -- " + height + "): ");
+                    var tem = Console.ReadLine();
+                    n = int.Parse(tem);
+                } while (n < low || n > height);
 
-            Agent agent = new Agent("test", 1000);
-            Console.WriteLine("This is the solution of the case above: ");
-            agent.solve(1000);
-            agent.printSolution();
+                while (repeat)
+                {
+                    Console.Clear();
+                    Generator g = new Generator(n, 0);
+                    Console.WriteLine("This is the level generated: ");
+                    printList(g.generate());
+                    Console.WriteLine("Do you want to change it? (y or n): ");
+                    string tem;
+                    do
+                    {
+                        tem = Console.ReadLine();
+                    } while (tem[0] != 'y' && tem[0] != 'n');
+                    repeat = (tem[0] == 'y');
+                }
 
-            Console.WriteLine("Would you like to add this level?(y or n): ");
-            string temp;
-            do
-            {
-                temp = Console.ReadLine();
-            } while (temp[0] != 'y' && temp[0] != 'n');
-            if(temp[0] == 'y')
-            {
-                Console.WriteLine("Add some notes: ");
-                string note = Console.ReadLine();
-                agent.save("test",note);
-                Console.WriteLine("level saved");
-            }
-            else
-            {
-                Console.WriteLine("Level aborted!");
-            }
+                Agent agent = new Agent("test", 100000);
+                Console.WriteLine("This is the solution of the case above: ");
+                agent.solve(100000);
+                agent.printSolution();
+
+                Console.WriteLine("Would you like to add this level?(y or n): ");
+                string temp;
+                do
+                {
+                    temp = Console.ReadLine();
+                } while (temp[0] != 'y' && temp[0] != 'n');
+                if (temp[0] == 'y')
+                {
+                    Console.WriteLine("Add some notes: ");
+                    string note = Console.ReadLine();
+                    agent.save("test", note);
+                    Console.WriteLine("level saved");
+                }
+                else
+                {
+                    Console.WriteLine("Level aborted!");
+                }
+
+                Console.WriteLine("Would you like to add more?");
+                string tempo;
+                do
+                {
+                    tempo = Console.ReadLine();
+                } while (tempo[0] != 'y' && tempo[0] != 'n');
+                rep = (tempo[0] == 'y');
+            } while (rep) ;
             Console.ReadLine();
         }
     }
